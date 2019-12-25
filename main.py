ef3909678c9c0c5dca0506c421069ef3fdc9a912
Szymon Wolszczak <szymon.wolszczak@tsh.io>
2019-12-25 22:03:38 +0100

    added some fancy blocks

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import numpy as npy
 import matplotlib.pyplot as plot
 
-npy.random.seed(20)
 
-N = 501
-T = 1
-dt = T / (N - 1)
-t = npy.linspace(dt, T, N)
-dX = npy.sqrt(dt) * npy.random.rand(1, N)
-X = npy.cumsum(dX, axis=1)
-dY = npy.sqrt(dt) * npy.random.rand(1, N)
-Y = npy.cumsum(dY, axis=1)
-fix, ax = plot.subplots()
-ax.set_title("Brownian motion")
-# generating X,Y point at chart
-ax.plot(X[0, :], Y[0, :])
-ax.plot(X[0, 0], Y[0, 0], 'ro')
-ax.plot(X[0, -1], Y[0, -1], 'yo')
-plot.tight_layout()
-plot.show()
+def var():
+    npy.random.seed(20)
+    N = 501
+    T = 1
+    dt = T / (N - 1)
+    t = npy.linspace(dt, T, N)
+    dX = npy.sqrt(dt) * npy.random.rand(1, N)
+    X = npy.cumsum(dX, axis=1)
+    dY = npy.sqrt(dt) * npy.random.rand(1, N)
+    Y = npy.cumsum(dY, axis=1)
+    fix, ax = plot.subplots()
+    ax.set_title("Brownian motion")
+    # generating X,Y point at chart
+    ax.plot(X[0, :], Y[0, :])
+    ax.plot(X[0, 0], Y[0, 0], 'ro')
+    ax.plot(X[0, -1], Y[0, -1], 'yo')
+    plot.tight_layout()
+    plot.show()
+
+
+def main():
+    var()
+
+
+if __name__ == '__main__':
+    main()
